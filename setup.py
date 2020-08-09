e801469405bef98677fb6dec2ad54f900de1f65e
Richard <rli14@berkeley.edu>
2020-08-09 02:15:43 -0400

    Init, works with bd

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="dcp", # Replace with your own username
+    version="0.0.1",
+    author="Yue Wang",
+    author_email="rli14@mit.edu",
+    description="Yue Wang DCP code",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6')
